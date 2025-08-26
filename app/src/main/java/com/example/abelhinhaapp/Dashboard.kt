b8e4db5f4ba966691c93203caafa52d2503bddbf
Estella <estella.carvalho_fernandes@proton.me>
2025-08-26 19:31:05 -0300

    Criando menu e implementando a tela inicial

--- app/src/main/java/com/example/abelhinhaapp/Dashboard.kt
+++ app/src/main/java/com/example/abelhinhaapp/Dashboard.kt
@@ -1,30 +1,20 @@
 package com.example.abelhinhaapp
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class Dashboard : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_dashboard)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        // Vai direto para a dashboard mas sem recursos online
-        val btnOffline: Button = findViewById<Button>(R.id.botao_offline)
-
-        btnOffline.setOnClickListener {
-            val intent = Intent(this, Dashboard::class.java)
-            startActivity(intent)
         }
     }
 }
